64d16fa3ac376a99ee74d0c81f5abf6da1de7674
inregist <inregist.irg@gmail.com>
2023-03-28 00:32:43 +0700

    scroll to tab center

--- src/features/Transaction/Transactions.tsx
+++ src/features/Transaction/Transactions.tsx
@@ -2,47 +2,55 @@ import { Layout } from '@flowmoni/components/Layout';
 import * as Tabs from '@radix-ui/react-tabs';
 import * as ScrollArea from '@radix-ui/react-scroll-area';
 import { TransactionGroup } from '@flowmoni/components/Transaction';
-import { useEffect, useState } from 'react';
+import { MouseEventHandler, useEffect, useState } from 'react';
+
+const months = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec',
+];
+
+const scrollToCenter: MouseEventHandler<HTMLButtonElement> = (event) => {
+  event.currentTarget.scrollIntoView({
+    behavior: 'smooth',
+    inline: 'center',
+  });
+};
+
+const generateTransactionGroups = () => {
+  return Array(5)
+    .fill(0)
+    .map((g) => ({
+      date: '2021-11-01',
+      transactions: Array(Math.floor(Math.random() * 5 + 1)).fill(0),
+    }));
+};
 
 export const Transactions = () => {
-  const generateTransactionGroups = () => {
-    return Array(5)
-      .fill(0)
-      .map((g) => ({
-        date: '2021-11-01',
-        transactions: Array(Math.floor(Math.random() * 5 + 1))
-          .fill(0)
-          .map((t) => ({})),
-      }));
-  };
-
   const [transactionGroups, setTransactionGroups] = useState<
     ReturnType<typeof generateTransactionGroups>
   >([]);
 
   useEffect(() => {
     setTransactionGroups(generateTransactionGroups());
+
+    const curMon = months[new Date().getMonth()] ?? '';
+    const curTab = document.getElementById(curMon);
+    curTab?.click();
   }, []);
 
   const handleTabChange = (value: string) => {
-    console.log('Tab Change', value);
     setTransactionGroups(generateTransactionGroups());
   };
-
-  const months = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec',
-  ];
 
   return (
     <Layout className="bg-white">
@@ -55,14 +63,16 @@ export const Transactions = () => {
           <Tabs.Root
             onValueChange={handleTabChange}
             className="h-12 w-full border-b border-gray-300 shadow-sm"
-            defaultValue="Nov"
+            defaultValue={months[new Date().getMonth()] ?? ''}
           >
             <Tabs.List className="flex w-full flex-nowrap border-b shadow-sm">
               {months.map((month, i) => (
                 <Tabs.Trigger
+                  id={month}
                   className="mb-1 h-12 w-2/5 flex-shrink-0 items-center justify-center border-slate-500 data-[state=active]:border-b-2 data-[state=active]:font-bold md:w-40"
                   key={month}
                   value={month}
+                  onClick={scrollToCenter}
                 >
                   {month}
                 </Tabs.Trigger>
@@ -81,8 +91,6 @@ export const Transactions = () => {
       <ScrollArea.Root className="h-1 w-full flex-grow bg-slate-200">
         <ScrollArea.Viewport className="h-full ">
           <div className="mb-4 flex w-full flex-col items-center bg-white p-4 shadow-md">
-            <div>Transaction Header</div>
-
             <button className="mt-2 w-64 rounded-full bg-green-500/20 p-2 font-semibold text-green-600">
               View Report
             </button>
